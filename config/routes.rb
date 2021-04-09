f3d7cdec4e82ff028b990b8f46aa7d38ae93c978
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-04-09 23:36:53 +0300

    UPDATE routing for heroku v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace 'api', defaults: { format: :json }, path: '/api' do
-    namespace 'v1' do
+    namespace 'v1', defaults: { format: :json }, path: '/v1'  do
       resources :users, only: [:create]
       resources :watchlists
       post "signup", to: "users#create"
