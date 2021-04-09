706f6083fcab52ace4170adcb6d85e5de9d30f2a
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-04-09 22:12:43 +0300

    ADD test for the models

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -3,27 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:subject) do
     described_class.new(
-      name: 'user_example',
+      username: 'user_example',
       password: 'password',
       password_confirmation: 'password'
     )
   end
 
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:username) }
 
     it 'is valid with valid attribute' do
       expect(subject).to be_valid
     end
 
     it 'The name of the user should exist' do
-      subject.name = ''
+      subject.username = ''
       expect(subject).not_to be_valid
     end
   end
 
   describe 'Associations', type: :model do
-    it { is_expected.to have_many(:articles) }
-    it { is_expected.to have_many(:votes) }
+    it { is_expected.to have_many(:watchlists) }
   end
 end
